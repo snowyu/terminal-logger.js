9ad38168ffc1c19a3adb25531603fc45da7d1d68
Riceball LEE <snowyu.lee@gmail.com>
2015-09-13 22:31:10 +0800

    * [bug]  duplcation instance are not the same one!

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 ConsoleLogger     = require('./lib')
-SingleLineLogger  =  require('./lib/single-line')
+SingleLineLogger  = require('./lib/single-line')
+defineProperty    = require('util-ex/lib/defineProperty')
+setPrototypeOf    = require('inherits-ex/lib/setPrototypeOf')
 
 module.exports = function (name, aOptions){
   var result = ConsoleLogger(name, aOptions)
-  result.single = SingleLineLogger(name, aOptions)
+  var single = result.single = {}
+  setPrototypeOf(single, result)
+  single.NEWLINE = ''
+  single._write = SingleLineLogger.prototype._write
+  // update this reference of the status.xxx methods.
+  single.updateColors(single._colors)
+  // defineProperty(result, 'single', undefined, {
+  //   get: function() {
+  //      return SingleLineLogger(this) //I do not like this.
+  //   }
+  // })
   return result
 }
